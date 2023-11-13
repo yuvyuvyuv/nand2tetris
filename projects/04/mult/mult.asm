81b897555c3edd936b1aefe7e72a9831a8307993
yuvyuvyuv <yuvyuv500@gmail.com>
2023-11-13 15:50:36 +0200

    unit 4 - mult - finish working

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -8,3 +8,35 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 //// Replace this comment with your code.
+
+// puts 0 in R2
+// loop R0 times
+//      R2 = R2 + R1 
+
+// adds 1+...+100.
+    @i // i refers to some mem. location.
+    M=1 // i=1
+    @sum // sum refers to some mem. location.
+    M=0 // sum=0
+(LOOP)
+    @i
+    D=M // D=i
+    @1
+    D=D-M // D=i-RAM[1]
+    @END
+    D;JGT // If (i-100)>0 goto END
+    @0
+    D=M // D=RAM[0]
+    @sum
+    M=D+M // sum=sum+i
+    @i
+    M=M+1 // i=i+1
+    @LOOP
+    0;JMP
+    @sum
+    D=M
+    @2
+    M=D
+(end)
+    @end
+    0;JMP
